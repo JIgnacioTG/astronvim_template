3ff17ff71f66b16c7d6d4ba71cdbcacf8605d120
José Ignacio Trava García <ignacio@reservamos.com>
2026-02-15 16:48:01 -0600

    chore: update astronvim v6

---
+++ lua/plugins/v6.lua
@@ -0,0 +1,7 @@
+---@type LazySpec
+return {
+  { "AstroNvim/astrocore", version = false, branch = "v3" },
+  { "AstroNvim/astrolsp", version = false, branch = "v4" },
+  { "AstroNvim/astroui", version = false, branch = "v4" },
+}
+
